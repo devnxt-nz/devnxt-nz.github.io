96539d478594273584e03bbebb913bda6748c424
Phil Wheeler <Phil-Wheeler@users.noreply.github.com>
2021-07-29 13:05:52 +1200

    Tidied up the speaker grid

---
+++ Client/Models/Presenter.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Presenter
+{
+    public String Name { get; set; }
+    public String Title { get; set; }
+
+    public String Social { get; set; }
+
+    public String Bio { get; set; }
+
+    public String Image { get; set; }
+}
